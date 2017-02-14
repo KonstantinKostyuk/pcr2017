b94b25236cff41c3af18531c04d08e6738172fa2
kostuk <kkostyuk@rus-robots.ru>
2017-02-14 18:09:16 +0300

    Test starter script update.

--- start_processors.sh
+++ start_processors.sh
@@ -4,3 +4,8 @@
 ~/pcr2017/app/modules/servo/ServoProcessor.py test &
 ~/pcr2017/app/modules/camcorder/FrontCamProcessor.py test &
 ~/pcr2017/app/modules/camcorder/PuckCamProcessor.py test &
+~/pcr2017/app/modules/LCD/LCD1602Processor.py test &
+~/pcr2017/app/modules/navigator/NavigationProcessor.py test &
+
+# flask server
+~/pcr2017/run.py
